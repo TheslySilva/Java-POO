95e8165ce575afb939f4f269a29aa35f1e32f055
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-29 21:28:51 -0300

    update src/estudopoo/Caneta.java

--- src/estudopoo/Caneta.java
+++ src/estudopoo/Caneta.java
@@ -1,31 +1,31 @@
 package estudopoo;
 
 public class Caneta{
-	
-	//Atributos da Classe
-	
-	String marca;
-	String cor;
-	float ponta;
-	int carga;
-	boolean tampada;
-	
-	//Metedos da Classe
-	
-	public void status(){
-		
-		System.out.println("Marca = " + this.marca);
-		System.out.println("Cor = "+ this.cor);
-		System.out.println("Ponta = " + this.ponta);
-		System.out.println("Carga = "+this.carga);
-		
-	}
-	public void Tampar(){
-		this.tampada=true;
-	}
-	
-	public void Destampar(){
-		this.tampada=false;
-	}
-	
+
+//Atributos da Classe
+
+String marca;
+String cor;
+float ponta;
+int carga;
+boolean tampada;
+
+//Metedos da Classe
+
+public void status(){
+
+System.out.println("Marca = " + this.marca);
+System.out.println("Cor = "+ this.cor);
+System.out.println("Ponta = " + this.ponta);
+System.out.println("Carga = "+this.carga);
+
+}
+public void Tampar(){
+this.tampada = true;
+}
+
+public void Destampar(){
+this.tampada = false;
+}
+
 }
