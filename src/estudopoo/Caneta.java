d9aa973c75507849c40531c01182a096809c9fb5
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-29 21:04:32 -0300

    update src/estudopoo/Caneta.java

---
+++ src/estudopoo/Caneta.java
@@ -0,0 +1,18 @@
+package estudopoo;
+
+public class Caneta{
+	
+	String marca;
+	String cor;
+	float ponta;
+	int carga;
+	
+	public void status(){
+		
+		System.out.println(marca);
+		System.out.println(cor);
+		System.out.println(ponta);
+		System.out.println(carga);
+		
+	}
+}
