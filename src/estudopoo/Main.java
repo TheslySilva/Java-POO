1cf420c720e880614b9fbb53210f6182f0a2ac4e
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-29 21:06:10 -0300

    update src/estudopoo/Main.java

--- src/estudopoo/Main.java
+++ src/estudopoo/Main.java
@@ -3,7 +3,11 @@ package estudopoo;
 public class Main{
 	 public static void main(String[] args) {
 	 	
-	 	
+	 	Caneta c1= new Caneta();
+	 	c1.marca= "Bic";
+	 	c1.cor="Preta";
+	 	c1.ponta= 0.7;
+	 	c1.carga= 100;
 	 	
 	}
 }
