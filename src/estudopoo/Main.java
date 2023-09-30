ef12ae9e9f0a6a96088abef39d7d611b72b14d7b
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-29 21:08:30 -0300

    update src/estudopoo/Main.java

--- src/estudopoo/Main.java
+++ src/estudopoo/Main.java
@@ -4,13 +4,18 @@ public class Main{
 	 public static void main(String[] args) {
 	 	
 	 	//Criando um Objeto apartir de uma Classe
+	 	
 	 	Caneta c1= new Caneta();
+	 	
 	 	//Atribuindo valores a ele
+	 	
 	 	c1.marca= "Bic";
 	 	c1.cor="Preta";
 	 	c1.ponta= 0.7;
 	 	c1.carga= 100;
+	 
 	 	//Metodo presente no Objeto
+	 	
 	 	c1.status();
 	 	
 	}
