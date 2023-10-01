e128aced1bb994a7f5d49f5ba6da60d9f523c557
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-01 15:37:13 -0300

    update Exercícios/exercicio01/Retangulo.java

--- Exercícios/exercicio01/Retangulo.java
+++ Exercícios/exercicio01/Retangulo.java
@@ -1,33 +1,38 @@
-package exercicio
+package exercicio01;
 
-public class Retangulo{
+public class Retangulo {
 	private double altura;
 	private double base;
 	
 	//Entrar valores
-	public void setAltura(double altura){
-		this.altura=altura;
+	public void setAltura(double altura) {
+		this.altura = altura;
 	}
-	public void setBase(double base){
-		this.base=base;
+	
+	public void setBase(double base) {
+		this.base = base;
 	}
+	
 	//Sair valores
-	private double getArea(){
-		double area= this.base*this.altura;
+	private double getArea() {
+		double area = this.base * this.altura;
 		return area;
 	}
-	private double getPerimetro(){
-		double perimetro= 2*(this.base+this.altura);
+	
+	private double getPerimetro() {
+		double perimetro = 2 * (this.base + this.altura);
 		return perimetro;
 	}
-	private double getDiagonal(){
-		double diagobal= Math.sqrt(Math.pow(this.base,2)+Math.pow(this.altura,2));
+	
+	private double getDiagonal() {
+		double diagonal = Math.sqrt(Math.pow(this.base, 2) + Math.pow(this.altura, 2));
 		return diagonal;
 	}
+	
 	//Saida completa
-	public String toString(){
-		getArea();
-		getPerimetro();
-		getDiagonal();
+	public String toString() {
+		return "\nArea = " + getArea() +
+		" \nPerimetro = " + getPerimetro() +
+		" \nDiagonal = " + getDiagonal();
 	}
 }
