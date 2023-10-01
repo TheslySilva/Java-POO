accb67ff3b51b424efb7dee3d583065576b97b96
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-01 16:12:33 -0300

    update Exercícios/exercicio01/Main.java

--- Exercícios/exercicio01/Main.java
+++ Exercícios/exercicio01/Main.java
@@ -1,32 +1,18 @@
 package exercicio01;
+
 import exercicio01.Retangulo;
 import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		Scanner sc= new Scanner(System.in);
-		Retangulo r= new Retangulo();
-		Retangulo r2= new Retangulo();
+		Retangulo r1 = new Retangulo();
 		
-		System.out.println("Digite os valores do Retangulo :");
-		System.out.print("\nBase : ");
-		r1.setBase(sc.nextDouble());
-		System.out.print("Altura : ");
-		r1.setAltura(sc.nextDouble());
+		System.out.println("Digite os valores : ");
+		r1.createRetangulo(sc.nextDouble(),sc.nextDouble());
 		
 		System.out.println(r1.toString());
-		
-		System.out.println("-----------------------");
-		
-		System.out.println("Digite os valores do Retangulo :");
-		System.out.print("\nBase : ");
-		r2.setBase(sc.nextDouble());
-		System.out.print("Altura : ");
-		r2.setAltura(sc.nextDouble());
-		
-		System.out.println(r2.toString());
-		
 	}
 }
