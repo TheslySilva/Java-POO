bee8fa1ab85893b3219754c2a56d0c5e1c74a9a2
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-03 11:50:43 -0300

    update Exercícios/exercicio04/Main.java

---
+++ Exercícios/exercicio04/Main.java
@@ -0,0 +1,20 @@
+package principal;
+
+import java.util.Scanner;
+import util.Conversor;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.print("Quanto esta o valor de um dollar? ");
+		double custo= sc.nextDouble();
+		System.out.print("Quantos dolares voce vai comprar? ");
+		double dollars= sc.nextDouble();
+		
+		double resultado = Conversor.converter(custo,dollars);
+		
+		System.out.printf("Valor que sera pago em reais = %.2f",resultado);
+	}
+}
