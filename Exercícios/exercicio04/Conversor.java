c683ed69e7bcdb5b721222823ef92dc3f714dd9d
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-03 11:51:11 -0300

    update Exercícios/exercicio04/Conversor.java

---
+++ Exercícios/exercicio04/Conversor.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Conversor {
+	
+	public static double converter(double valor,double quantidade) {
+		double conversao= (valor*6.0)/100.0;
+		return  (valor+conversao)*quantidade;
+	}
+	
+}
