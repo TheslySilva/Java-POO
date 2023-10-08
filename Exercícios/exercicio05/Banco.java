74bbba5472c48175a8db5c8c4ae0275a42b00e5a
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-08 11:08:15 -0300

    update Exercícios/exercicio05/Banco.java

---
+++ Exercícios/exercicio05/Banco.java
@@ -0,0 +1,47 @@
+package main;
+
+public class Banco {
+	private int conta;
+	private String nome;
+	private double saldo;
+	
+	//Construtor
+	public Banco(int conta, String nome, double saldo) {
+		this.conta = conta;
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+	
+	public Banco(int conta, String nome) {
+		this.conta = conta;
+		this.nome = nome;
+	}
+	
+	//Get e Set
+	private void setConta(int conta) {
+		this.conta = conta;
+	}
+	
+	private int getConta() {
+		return this.conta;
+	}
+	
+	//
+	private void setNome(String nomr) {
+		this.nome = nome;
+	}
+	
+	private String getNome() {
+		return this.nome;
+	}
+	
+	//Deposito e Saque
+	public void deposito(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void  saque(double saldo) {
+		this.saldo=this.saldo-saldo;
+		this.saldo= this.saldo-5;
+	}
+}
