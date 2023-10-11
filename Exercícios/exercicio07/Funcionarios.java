44aaafdf6c162d6d5427dda763486084a74ededc
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-11 09:02:37 -0300

    update Exercícios/exercicio07/Funcionarios.java

---
+++ Exercícios/exercicio07/Funcionarios.java
@@ -0,0 +1,17 @@
+package exercicio07;
+
+public class Funcionario {
+	
+	private int id;
+	private String nome;
+	private double salario;
+	//Construtor
+	public Funcionario(int id, String nome, double salario){
+		
+		this.id=id;
+		this.nome=nome;
+		this.salario=salario;
+	}
+	//Get e Set
+	
+}
