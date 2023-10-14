de61666afc740c94107460c8164e1038aab23472
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-14 11:42:22 -0300

    update Exercícios/exercicio08/entities/Cliente.java

---
+++ Exercícios/exercicio08/entities/Cliente.java
@@ -0,0 +1,42 @@
+package exercicio08.entities;
+
+import java.util.Date;
+
+import exercicio08.entities.Cliente;
+import java.text.SimpleDateFormat;
+
+public class Cliente {
+	
+	public static  final SimpleDateFormat formatarData= new SimpleDateFormat("dd/MM/yyyy");
+	//
+	private String nome;
+	private String email;
+	private Date aniversario;
+	//
+	public void Cliente(String nome, String email, Date aniversario) {
+		this.nome = nome;
+		this.email = email;
+		this.aniversario = aniversario;
+	}
+	
+	//
+	public String getNome() {
+		return this.nome;
+	}
+	
+	//
+	public String getEmail() {
+		return this.email;
+	}
+	
+	//
+	public Date getAniversario() {
+		return this.aniversario;
+	}
+	//
+	public String toString(){
+		
+		return "nome "+this.nome+", ("
+		+formatarData.format(this.aniversario)+") - "+this.email;
+	}
+}
