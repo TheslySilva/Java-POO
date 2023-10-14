659c74ca8c6a65b80176f908619d82c559497f8d
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-14 11:41:34 -0300

    update Exercícios/exercicio08/programa/Main.java

---
+++ Exercícios/exercicio08/programa/Main.java
@@ -0,0 +1,19 @@
+package exercicio08.programa;
+
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc= new Scanner(System.in);
+		SimpleDateFormat formatarData= new SimpleDateFormat();
+		//
+		
+		
+	}
+}
