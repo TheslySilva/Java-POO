375f0d1e9f5890d018dc7ff9257fd96da689c865
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-14 12:21:34 -0300

    update Exercícios/exercicio08/programa/Main.java

--- Exercícios/exercicio08/programa/Main.java
+++ Exercícios/exercicio08/programa/Main.java
@@ -12,12 +12,14 @@ public class Main {
 		
 		Scanner sc= new Scanner(System.in);
 		SimpleDateFormat formatarData= new SimpleDateFormat();
+		
 		//Dados do cliente
+		
 		System.out.println("Entre com os dados do cliente : ");
 		System.out.print("\nNome : ");
 		String nome= sc.nextLine();
 		//
-		sc.nextLine();
+		sc.nextLine();//limpar quebra de linha
 		//
 		System.out.print("Email : ");
 		String email= sc.nextLine();
@@ -28,15 +30,29 @@ public class Main {
 		Cliente cliente = new cliente(nome,email,aniversario);
 		
 		//Dados do estoque
+		
 		System.out.println("Entre com os dados do estoque :");
 		System.out.print("Estado : ");
 		StatusDoProduto status = StatusDoProduto.valueOf(sc.next());
+		
 		//Adicionando items ao pedido
+		
 		System.out.print("Quantos itens para este pedido?");
 		int n= sc.nextInt():
 		
 		for(int i = 0; i<n; i++){
 			
+			System.out.println("Entre com os dados do #"+(i+1)+" produto :");
+			
+			System.out.print("\nNome do produto : ");
+			sc.nextLine();//limpar quebra de linha
+			String nome = sc.nextLine();
+			
+			System.out.print("Preço do produto : ");
+			double preco= sc.nextDouble();
+			
+			System.out.print("Quantidade : ");
+			int quantidade= sc.nextInt();
 		}
 	}
 }
