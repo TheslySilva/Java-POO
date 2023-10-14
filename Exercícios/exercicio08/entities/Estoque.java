d172bdb2c8eecc431b64cf842c7aa6fca9b68a9f
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-14 11:42:56 -0300

    update Exercícios/exercicio08/entities/Estoque.java

---
+++ Exercícios/exercicio08/entities/Estoque.java
@@ -0,0 +1,64 @@
+package exercicio08.entities;
+
+import java.util.Date;
+import exercicio08.enums.StatusDoPedido;
+import exercicio08.entities.Cliente;
+import java.util.List;
+import exercicio08.entities.ItemPedido;
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+
+public class Estoque {
+	SimpleDateFormat formatarData = new SimpleDateFormat("dd/MM/yyyy");
+	private Date momento;
+	private StatusDoPedido estado;
+	//
+	private Cliente cliente;
+	private List<ItemPedido> listaDeItems = new ArrayList<ItemPedido>();
+	
+	//Construtor
+	public void Estoque(Date momento, StatusDoPedido estado, Cliente cliente) {
+		this.momento = momento;
+		this.estado = estado;
+		this.cliente = cliente;
+	}
+	
+	//Adicionar e remover
+	public void addItem(ItemPedido item) {
+		this.listaDeItems.add(item);
+	}
+	
+	public void removeItem(ItemPedido item) {
+		this.listaDeItems.remove(item);
+	}
+	
+	//Contas
+	public double total() {
+		double soma = 0;
+		for (ItemPedido lista : listaDeItems) {
+			soma += lista.somaTotal();
+		}
+		return soma;
+	}
+	
+	//
+	public String toString() {
+		
+		StringBuilder str = new StringBuilder();
+		str.append("Momento do Estoque : ");
+		str.append(formatarData.format(momento) + "\n");
+		str.append("Status do produto : ");
+		str.append(estado + "\n");
+		str.append("Cliente : ");
+		str.append(cliente + "\n");
+		str.append("Itens pedido : ");
+		
+		for (ItemPedido items : listaDeItems) {
+			str.append(items + "\n");
+		}
+		str.append("Valor total : ");
+		str.append(String.format("%.2f%n", total()));
+		
+		return str.toString();
+	}
+}
