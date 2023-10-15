1a0b89f9f09d90f5d4f22bdc980bb2f7949a0ed7
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-15 17:27:35 -0300

    update Exercícios/exercicio10/entities/Pessoa.java

---
+++ Exercícios/exercicio10/entities/Pessoa.java
@@ -0,0 +1,29 @@
+package entities;
+
+public abstract class Pessoa {
+	private String nome;
+	private Double rendaAnual;
+	
+	//
+	//Construtor
+	//
+	public Pessoa(String nome, Double rendaAnual) {
+		this.nome = nome;
+		this.rendaAnual = rendaAnual;
+	}
+	//
+	//Gets
+	//
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getRendaAnual() {
+		return rendaAnual;
+	}
+	//
+	//Metedo Abstrato
+	//
+	public abstract  Double imposto();
+	
+}
