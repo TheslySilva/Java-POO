9f8d7133c3091efebfeaaaa6bff362a4e70f1c83
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-16 11:45:52 -0300

    update Exercícios/exercicio11/programa/Main.java

--- Exercícios/exercicio11/programa/Main.java
+++ Exercícios/exercicio11/programa/Main.java
@@ -1,38 +1,47 @@
 package exercicio11.programa;
 
 import java.util.Scanner;
+import exercicio11.models.entities.ContaBancaria;
+import exercicio11.models.exceptions.SaqueException;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		//Dadoa do Titular
-		
-		System.out.println("Entre com os dados da conta:");
-		
-		System.out.print("\nNumero da conta: ");
-		int numero= sc.nextInt();
-		
-		sc.nextLine();
-		
-		System.out.print("Titular: ");
-		String titular=sc.nextLine();
-		
-		System.out.print("Saldo inicial: ");
-		double saldo = sc.nextDouble();
-		
-		System.out.print("Limite de saque: ");
-		double limiteDeSaque= sc.nextDouble;
-		
-		ContaBancaria pessoa1 = new ContaBancaria(numero,titular,saldo,limiteDeSaque);
-		
-		//Saques e Depositos
-		
-		System.out.print("\nEntre com o valor do saque: ");
-		p1.saque(sc.nextDouble());
-		
-		System.out.println();
+		try {
+			//Dadoa do Titular
+			System.out.println("Entre com os dados da conta:");
+			
+			System.out.print("\nNumero da conta: ");
+			int numero = sc.nextInt();
+			
+			sc.nextLine();
+			
+			System.out.print("Titular: ");
+			String titular = sc.nextLine();
+			
+			System.out.print("Saldo inicial: ");
+			double saldo = sc.nextDouble();
+			
+			System.out.print("Limite de saque: ");
+			double limiteDeSaque = sc.nextDouble();
+			
+			ContaBancaria pessoa1 = new ContaBancaria(numero, titular, saldo, limiteDeSaque);
+			
+			//Saques e Depositos
+			System.out.print("\nEntre com o valor do saque: ");
+			double saque = sc.nextDouble();
+			
+			pessoa1.saque(saque);
+			
+			System.out.println(pessoa1.informacoes());
+			
+		} catch (SaqueException e) {
+			
+			System.out.println("\nErro ao realizar saque: " + e.getMessage());
+			
+		}
 	}
 }
