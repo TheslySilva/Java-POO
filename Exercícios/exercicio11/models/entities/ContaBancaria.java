ac1590c7cab8d0fb0486441f167a8e8cfa150102
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-16 09:57:02 -0300

    update Exercícios/exercicio11/models/entities/ContaBancaria.java

--- Exercícios/exercicio11/models/entities/ContaBancaria.java
+++ Exercícios/exercicio11/models/entities/ContaBancaria.java
@@ -1,27 +1,28 @@
-package exercicio11.model.entities;
+package exercicio11.models.entities;
 
 public class ContaBancaria {
-	
 	private Integer numero;
 	private String dono;
 	private Double saldo;
 	private Double limiteDeSaque;
+	
 	//
 	//Construtor
 	//
-	public ContaBancaria(
+	public ContaBancaria (Integer numero, String dono, Double saldo, Double limiteDeSaque) {
 		
-	Integer numero,
-	String dono,
-	Double saldo, 
-	Double limiteDeSaque
-	
-	){
-		
-		this.numero=numero;
-		this.dono=dono;
-		this.saldo=saldo;
-		this.limiteDeSaque=limiteDeSaque;
-		
+		this.numero = numero;
+		this.dono = dono;
+		this.saldo = saldo;
+		this.limiteDeSaque = limiteDeSaque;
+	}
+	//
+	//Gets e Sets
+	//
+	public Integer getNumero(){
+		return this.numero;
+	}
+	public String getDono(){
+		return this.dono;
 	}
 }
