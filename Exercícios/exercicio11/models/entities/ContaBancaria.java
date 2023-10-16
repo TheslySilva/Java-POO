d329de11b21d3fbf5b7dac1484003fb60146dbda
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-16 09:38:46 -0300

    update Exercícios/exercicio11/models/entities/ContaBancaria.java

---
+++ Exercícios/exercicio11/models/entities/ContaBancaria.java
@@ -0,0 +1,14 @@
+package exercicio11.model.entities;
+
+public class ContaBancaria {
+	
+	private Integer numero;
+	private String dono;
+	private Double saldo;
+	private Double limiteDeSaque;
+	//
+	//Construtor
+	//
+	public ContaBancaria(){
+	}
+}
