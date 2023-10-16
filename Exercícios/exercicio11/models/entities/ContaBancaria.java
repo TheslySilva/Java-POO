d3d0fe4713d89b0f6f5b0c4fe3ef8531af506b93
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-16 11:45:27 -0300

    update Exercícios/exercicio11/models/entities/ContaBancaria.java

--- Exercícios/exercicio11/models/entities/ContaBancaria.java
+++ Exercícios/exercicio11/models/entities/ContaBancaria.java
@@ -1,14 +1,16 @@
 package exercicio11.models.entities;
 
+import exercicio11.models.exceptions.SaqueException;
+
 public class ContaBancaria {
+	
 	private Integer numero;
 	private String titular;
 	private Double saldo;
 	private Double limiteDeSaque;
 	
 	//Construtor
-	
-	public ContaBancaria (Integer numero, String titular, Double saldo, Double limiteDeSaque) {
+	public ContaBancaria(Integer numero, String titular, Double saldo, Double limiteDeSaque) {
 		
 		this.numero = numero;
 		this.titular = titular;
@@ -17,27 +19,42 @@ public class ContaBancaria {
 	}
 	
 	//Gets e Sets
-	
-	public Integer getNumero(){
+	public Integer getNumero() {
 		return this.numero;
 	}
-	public String gettitular(){
+	
+	public String gettitular() {
 		return this.titular;
 	}
-	public Double getSaldo(){
-		return this.saldo:
+	
+	public Double getSaldo() {
+		return this.saldo;
 	}
-	public Double getLimiteDeSaque(){
+	
+	public Double getLimiteDeSaque() {
 		return this.limiteDeSaque;
 	}
 	
 	//Metodos personalizados
-	
-	public void deposito throws (Double valor){
-		this.saldo+= valor;
-	}
-	public void saque(Double valor){
-		this.saldo-= valor;
+	public void deposito(Double valor) {
+		
 	}
 	
+	public void saque(Double valor) throws SaqueException {
+		
+		if (saldo < valor) {
+			
+			throw new SaqueException ("Saldo insulficiente!");
+			
+		} else if (valor > limiteDeSaque) {
+			
+			throw new SaqueException("Limite de saque insulficiente!");
+			
+		} 
+			this.saldo -= valor;	
+		
+	}
+	public String informacoes(){
+		return "\nSaldo atualizado: "+String.format("$%.2f",saldo);
+	}
 }
