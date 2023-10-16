02137cca4f8b4f0e0ddd29b03d4f091c3cc08a1d
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-10-16 10:18:05 -0300

    update Exercícios/exercicio11/programa/Main.java

--- Exercícios/exercicio11/programa/Main.java
+++ Exercícios/exercicio11/programa/Main.java
@@ -1,8 +1,35 @@
 package exercicio11.programa;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		//Dadoa do Titular
+		
+		System.out.println("Entre com os dados da conta:");
+		
+		System.out.print("Numero da conta: ");
+		int numero= sc.nextInt();
+		
+		sc.nextLine();
+		
+		System.out.print("Titular: ");
+		String titular=sc.nextLine();
+		
+		System.out.print("Saldo inicial: ");
+		double saldo = sc.nextDouble();
+		
+		System.out.print("Limite de saque: ");
+		double limiteDeSaque= sc.nextDouble;
+		
+		ContaBancaria pessoa1 = new ContaBancaria(numero,titular,saldo,limiteDeSaque);
+		
+		//Saques e Depositos
+		
 		
 	}
 }
